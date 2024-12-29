cfa054fcee882e5d86acb5ccbd2c15c02ac741c9
Citer <citer5@hotmail.com>
2024-12-28 23:10:01 -0600

    deshabilitacion de font-awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+createApp(App)
+	.component("font-awesome-icon", FontAwesomeIcon)
+	.use(router)
+	.mount("#app");
