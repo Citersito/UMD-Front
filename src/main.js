c2759f700747bb729fb831cd21874d62a13231fc
Citer <citer5@hotmail.com>
2024-12-30 01:13:53 -0600

    correcion de errores

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 createApp(App)
-	.component("font-awesome-icon", FontAwesomeIcon)
 	.use(router)
 	.mount("#app");
